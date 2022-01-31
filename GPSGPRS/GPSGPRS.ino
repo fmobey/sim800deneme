0fc6b59a1958c3e1c15ec0c1ba5d727a73f53cb0
Furkan metin OĞUZ <dumstp1@gmail.com>
2022-01-31 13:39:28 +0300

    asd

--- GPSGPRS/GPSGPRS.ino
+++ GPSGPRS/GPSGPRS.ino
@@ -1,58 +1,8 @@
-/*
-Autour: Furkan Metin OĞUZ
-Date:2022
-**/
- 
-#include <TinyGPS++.h> 
-#include <HardwareSerial.h> 
+#include <TinyGPS++.h> // Library über http://arduiniana.org/libraries/tinygpsplus/ downloaden und installieren
+#include <HardwareSerial.h> // sollte bereits mit Arduino IDE installiert sein
 #include "EEPROM.h" 
-#include <Wire.h>
-#include <TinyGsmClient.h>
-#include <PubSubClient.h>
-#include <Adafruit_Sensor.h>
-#include <ArduinoJson.h>
-
-// TTGO T-Call pins
-#define MODEM_RST            5
-#define MODEM_PWKEY          4
-#define MODEM_POWER_ON       23
-#define MODEM_TX             27
-#define MODEM_RX             26
-#define I2C_SDA              21
-#define I2C_SCL              22
-
-#define IP5306_ADDR          0x75
-#define IP5306_REG_SYS_CTL0  0x00
-
-#define IP5306_ADDR          0x75
-#define IP5306_REG_SYS_CTL0  0x00
 #define EEPROM_SIZE 1024
-#define TASK_SERIAL_RATE 1000 // ms
-uint32_t nextSerialTaskTs = 0;
-uint32_t nextOledTaskTs = 0;
- 
 
-
-// Modeminizi seçin:
-#define TINY_GSM_MODEM_SIM800 
-
-#define SerialMon Serial
-#define SerialAT Serial1
-
-#define TINY_GSM_DEBUG SerialMon
-
-// varsa, GSM PIN'i ayarlayın
-#define GSM_PIN ""
-#ifdef DUMP_AT_COMMANDS
-  #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
-  TinyGsm modem(debugger);
-#else
-  TinyGsm modem(SerialAT);
-#endif
-
-TinyGsmClient client(modem);
-PubSubClient mqtt(client);
 HardwareSerial SerialGPS(2);
 
 TinyGPSPlus gps;
@@ -68,24 +18,36 @@ struct GpsDataState_t {
   double prevDist = 0;
 };
 GpsDataState_t gpsState = {};
-//batarya
-const int giris=100;
-float total =0;
-float average=0;
-float okunandeger[giris];
-float deger =0;
-float volt =0;
-float yuzde =-1;
 
-// Varsa GPRS kimlik bilgileriniz
-const char apn[] = "internet"; 
+#define TASK_SERIAL_RATE 1000 // ms
+uint32_t nextSerialTaskTs = 0;
+uint32_t nextOledTaskTs = 0;
+ 
+
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800 // Modem is SIM800L
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands
+#define SerialAT Serial1
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+// set GSM PIN, if any
+#define GSM_PIN ""
+
+// Your GPRS credentials, if any
+const char apn[] = "internet"; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
 const char gprsUser[] = "";
 const char gprsPass[] = "";
 
-// SIM card PIN 
+// SIM card PIN (leave empty, if not defined)
 const char simPIN[]   = ""; 
 
-// MQTT detay
+// MQTT details
 const char* broker = "194.31.59.188";                    // Public IP address or domain name
 const char* mqttUsername = "deneme";  // MQTT username
 const char* mqttPassword = "deneme";  // MQTT password
@@ -95,30 +57,75 @@ const char* topicOutput = "v1/devices/me/telemetry";
 const char* topic = "v1/devices/me/telemetry";
 
 
+// Define the serial console for debug prints, if needed
+//#define DUMP_AT_COMMANDS
 
+#include <Wire.h>
+#include <TinyGsmClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <ArduinoJson.h>
+TinyGsmClient client(modem);
+PubSubClient mqtt(client);
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
 
 // BME280 pins
 
 uint32_t lastReconnectAttempt = 0;
 
+const int giris=100;
+float total =0;
+float average=0;
+float okunandeger[giris];
+float deger =0;
+float volt =0;
+float yuzde =-1;
 
+String imeiString="başarısız imei ";
 
-
-
-
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
 
 long lastMsg = 0;
 
 
-void mqttCallback(char* topic, byte* message, unsigned int len) {
+
+
+
+void mqttCallback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
+  String messageTemp;
   
-
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
   Serial.println();
 
-// mqtt üzerinden kilit açıp kapatma
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
   if (String(topic) == "v1/devices/me/telemetry") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
@@ -136,7 +143,10 @@ boolean mqttConnect() {
   SerialMon.print("Connecting to ");
   SerialMon.print(broker);
 
- 
+  // Connect to MQTT Broker without username and password
+  //boolean status = mqtt.connect("GsmClientN");
+
+  // Or, if you want to authenticate MQTT:
   boolean status = mqtt.connect(mqtt_id, mqttUsername, mqttPassword);
 
   if (status == false) {
@@ -153,13 +163,13 @@ boolean mqttConnect() {
 
 
 void setup() {
-  //SerialMon.begin(9600, SERIAL_8N1, 12, 13); kilit sistemi kullanılcağı zaman 
+  // Set console baud rate
   SerialMon.begin(9600);
   delay(10);
-  SerialMon.begin(9600, SERIAL_8N1, 13, 12);//dinleme
-
   
 
+  
+  // Set modem reset, enable, power pins
   pinMode(MODEM_PWKEY, OUTPUT);
   pinMode(MODEM_RST, OUTPUT);
   pinMode(MODEM_POWER_ON, OUTPUT);
@@ -171,11 +181,12 @@ void setup() {
   
   SerialMon.println("Wait...");
 
-  // Gsm seri port
+  // Set GSM module baud rate and UART pins
   SerialAT.begin(9600, SERIAL_8N1, MODEM_RX, MODEM_TX);
   delay(6000);
 
-
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
   SerialMon.println("Initializing modem...");
   modem.restart();
   // modem.init();
@@ -184,25 +195,13 @@ void setup() {
   SerialMon.print("Modem Info: ");
   SerialMon.println(modemInfo);
 
-  // sim kilit açma
+  // Unlock your SIM card with a PIN if needed
   if ( GSM_PIN && modem.getSimStatus() != 3 ) {
     modem.simUnlock(GSM_PIN);
   }
 
-  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
-  uint8_t imeiLen = modem.getIMEI(imei);
-  if (imeiLen > 0) {
-    Serial.print("SIM card IMEI: "); Serial.println(imei);
-      Serial.print("SIM card IMEI: "); Serial.println(imei);
-        Serial.print("SIM card IMEI: "); Serial.println(imei);
-          Serial.print("SIM card IMEI: "); Serial.println(imei);
-            Serial.print("SIM card IMEI: "); Serial.println(imei);
-              Serial.print("SIM card IMEI: "); Serial.println(imei);
-                Serial.print("SIM card IMEI: "); Serial.println(imei);
-                
-  }
 
-
+  imeiString=   modem.getIMEI();//burdayım
   SerialMon.print("Connecting to APN: ");
   SerialMon.print(apn);
   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
@@ -276,12 +275,12 @@ void loop() {
     return;
   }
 
-  // Modülden GPS koordinatlarını okuma
+  // GPS Koordinaten von Modul lesen
   gpsState.originLat = gps.location.lat();
   gpsState.originLon = gps.location.lng();
   gpsState.originAlt = gps.altitude.meters();
  
-  // Geçerli konumu kalıcı ESP32 belleğine yaz
+  // Aktuelle Position in nichtflüchtigen ESP32-Speicher schreiben
   long writeValue;
   writeValue = gpsState.originLat * 1000000;
   EEPROM_writeAnything(0, writeValue);
@@ -289,7 +288,7 @@ void loop() {
   EEPROM_writeAnything(4, writeValue);
   writeValue = gpsState.originAlt * 1000000;
   EEPROM_writeAnything(8, writeValue);
-  EEPROM.commit(); 
+  EEPROM.commit(); // erst mit commit() werden die Daten geschrieben
  
   gpsState.distMax = 0;
   gpsState.altMax = -999999;
@@ -323,8 +322,8 @@ void loop() {
   long now = millis();
   if (now - lastMsg > 3000) {
     lastMsg = now;
-
-     for(int i=0; i<100;i++){
+    
+       for(int i=0; i<100;i++){
   deger = analogRead(34);
   
   volt = ((deger/1202)*345)/15;
@@ -340,9 +339,10 @@ void loop() {
 
 
     StaticJsonDocument < 256 > JSONbuffer;
-    JsonObject GpsData = JSONbuffer.createNestedObject();
+     JsonObject GpsData = JSONbuffer.createNestedObject();
     JsonObject BatteryData = JSONbuffer.createNestedObject();
-
+    JsonObject imeiData = JSONbuffer.createNestedObject();
+    imeiData["imei"]=  imeiString;
       BatteryData["BatteryVoltage"]=average;
       BatteryData["BatteryPercent"]=yuzde;
 
@@ -351,11 +351,13 @@ void loop() {
         GpsData["SPEED"] = gps.speed.kmph();
         GpsData["ALT"] = gps.altitude.meters();
         GpsData["LONG"] = gps.location.lng();
-        
     char JSONmessageBuffer[200];
     serializeJsonPretty(JSONbuffer, JSONmessageBuffer);
     Serial.println("Sending message to MQTT topic..");
     Serial.println(JSONmessageBuffer);
+    if(  mqtt.subscribe(topicOutput)==true ){
+
+    }
     if (mqtt.publish("v1/devices/me/telemetry", JSONmessageBuffer) == true) {
       Serial.println("Success sending message");
     } else {
@@ -366,7 +368,4 @@ void loop() {
 
 
   mqtt.loop();
-}
-
-
-  }
+}}
