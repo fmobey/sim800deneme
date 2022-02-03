b2fcf41810fd3ed21882b1a243cf97cb49a0b5e8
Furkan metin OĞUZ <dumstp1@gmail.com>
2022-02-03 17:27:48 +0300

    .

--- Yeni Metin Belgesi.ino
+++ Yeni Metin Belgesi.ino
@@ -1,107 +1,81 @@
-#include <TinyGPS++.h> // Library über http://arduiniana.org/libraries/tinygpsplus/ downloaden und installieren
-#include <HardwareSerial.h> // sollte bereits mit Arduino IDE installiert sein
-#include "EEPROM.h" 
-#define EEPROM_SIZE 1024
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
 
-HardwareSerial SerialGPS(2);
-
-TinyGPSPlus gps;
-struct GpsDataState_t {
-  double originLat = 0;
-  double originLon = 0;
-  double originAlt = 0;
-  double distMax = 0;
-  double dist = 0;
-  double altMax = -999999;
-  double altMin = 999999;
-  double spdMax = 0;
-  double prevDist = 0;
-};
-GpsDataState_t gpsState = {};
-
-#define TASK_SERIAL_RATE 1000 // ms
-uint32_t nextSerialTaskTs = 0;
-uint32_t nextOledTaskTs = 0;
- 
-
-
-// Select your modem:
-#define TINY_GSM_MODEM_SIM800 // Modem is SIM800L
-
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define SerialMon Serial
-// Set serial for AT commands
-#define SerialAT Serial1
-
-// Define the serial console for debug prints, if needed
-#define TINY_GSM_DEBUG SerialMon
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[] = "internet"; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-// SIM card PIN (leave empty, if not defined)
-const char simPIN[]   = ""; 
-
-// MQTT details
-const char* broker = "194.31.59.188";                    // Public IP address or domain name
-const char* mqttUsername = "deneme";  // MQTT username
-const char* mqttPassword = "deneme";  // MQTT password
-const char* mqtt_id = "deneme";  // MQTT id
-const char* topicOutput = "v1/devices/me/telemetry";
-
-const char* topic = "v1/devices/me/telemetry";
-
-
-// Define the serial console for debug prints, if needed
-//#define DUMP_AT_COMMANDS
-
-#include <Wire.h>
-#include <TinyGsmClient.h>
-
-#ifdef DUMP_AT_COMMANDS
-  #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
-  TinyGsm modem(debugger);
-#else
-  TinyGsm modem(SerialAT);
-#endif
-
+#include <WiFi.h>
 #include <PubSubClient.h>
-#include <Adafruit_Sensor.h>
+#include <Wire.h>
 #include <Adafruit_BME280.h>
-#include <ArduinoJson.h>
-TinyGsmClient client(modem);
-PubSubClient mqtt(client);
+#include <Adafruit_Sensor.h>
 
-// TTGO T-Call pins
-#define MODEM_RST            5
-#define MODEM_PWKEY          4
-#define MODEM_POWER_ON       23
-#define MODEM_TX             27
-#define MODEM_RX             26
-#define I2C_SDA              21
-#define I2C_SCL              22
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-// BME280 pins
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
 
-uint32_t lastReconnectAttempt = 0;
-
-
-
-
-
-#define IP5306_ADDR          0x75
-#define IP5306_REG_SYS_CTL0  0x00
-
+WiFiClient espClient;
+PubSubClient client(espClient);
 long lastMsg = 0;
+char msg[50];
+int value = 0;
 
+//uncomment the following lines if you're using SPI
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
 
-void mqttCallback(char* topic, byte* message, unsigned int length) {
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+float temperature = 0;
+float humidity = 0;
+
+// LED Pin
+const int ledPin = 4;
+
+void setup() {
+  Serial.begin(115200);
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  //status = bme.begin();  
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
@@ -117,233 +91,67 @@ void mqttCallback(char* topic, byte* message, unsigned int length) {
 
   // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
   // Changes the output state according to the message
-  if (String(topic) == "v1/devices/me/telemetry") {
+  if (String(topic) == "esp32/output") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
       Serial.println("on");
-      
+      digitalWrite(ledPin, HIGH);
     }
     else if(messageTemp == "off"){
       Serial.println("off");
+      digitalWrite(ledPin, LOW);
     }
   }
-  }
-
-
-boolean mqttConnect() {
-  SerialMon.print("Connecting to ");
-  SerialMon.print(broker);
-
-  // Connect to MQTT Broker without username and password
-  //boolean status = mqtt.connect("GsmClientN");
-
-  // Or, if you want to authenticate MQTT:
-  boolean status = mqtt.connect(mqtt_id, mqttUsername, mqttPassword);
-
-  if (status == false) {
-    SerialMon.println(" fail");
-    ESP.restart();
-    return false;
-  }
-  SerialMon.println(" success");
-  mqtt.subscribe(topicOutput);
-  
-
-  return mqtt.connected();
 }
 
-
-void setup() {
-  // Set console baud rate
-  SerialMon.begin(9600);
-  delay(10);
-  
-
-  
-
-  // Set modem reset, enable, power pins
-  pinMode(MODEM_PWKEY, OUTPUT);
-  pinMode(MODEM_RST, OUTPUT);
-  pinMode(MODEM_POWER_ON, OUTPUT);
-  digitalWrite(MODEM_PWKEY, LOW);
-  digitalWrite(MODEM_RST, HIGH);
-  digitalWrite(MODEM_POWER_ON, HIGH);
-  
-
-  
-  SerialMon.println("Wait...");
-
-  // Set GSM module baud rate and UART pins
-  SerialAT.begin(9600, SERIAL_8N1, MODEM_RX, MODEM_TX);
-  delay(6000);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  SerialMon.println("Initializing modem...");
-  modem.restart();
-  // modem.init();
-
-  String modemInfo = modem.getModemInfo();
-  SerialMon.print("Modem Info: ");
-  SerialMon.println(modemInfo);
-
-  // Unlock your SIM card with a PIN if needed
-  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(GSM_PIN);
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
-
-
-
-  SerialMon.print("Connecting to APN: ");
-  SerialMon.print(apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    SerialMon.println(" fail");
-    ESP.restart();
-  }
-  else {
-    SerialMon.println(" OK");
-  }
-  
-  if (modem.isGprsConnected()) {
-    SerialMon.println("GPRS connected");
-  }
-
-  // MQTT Broker setup
-  mqtt.setServer(broker, 1883);
-  mqtt.setCallback(mqttCallback);
-
-  //gps
-    SerialGPS.begin(9600, SERIAL_8N1, 18, 19);
-
-  while (!EEPROM.begin(EEPROM_SIZE)) {
-    
-  }
- 
- 
-  long readValue;
-  EEPROM_readAnything(0, readValue);
-  gpsState.originLat = (double)readValue / 1000000;
- 
-  EEPROM_readAnything(4, readValue);
-  gpsState.originLon = (double)readValue / 1000000;
- 
-  EEPROM_readAnything(8, readValue);
-  gpsState.originAlt = (double)readValue / 1000000;
-}
-template <class T> int EEPROM_writeAnything(int ee, const T& value)
-{
-  const byte* p = (const byte*)(const void*)&value;
-  int i;
-  for (i = 0; i < sizeof(value); i++)
-    EEPROM.write(ee++, *p++);
-  return i;
-}
- 
-template <class T> int EEPROM_readAnything(int ee, T& value)
-{
-  byte* p = (byte*)(void*)&value;
-  int i;
-  for (i = 0; i < sizeof(value); i++)
-    *p++ = EEPROM.read(ee++);
-  return i;
 }
 void loop() {
-
-  if (!mqtt.connected()) {
-    SerialMon.println("=== MQTT NOT CONNECTED ===");
-
-
-
-    
-    // Reconnect every 10 seconds
-    uint32_t t = millis();
-    if (t - lastReconnectAttempt > 1000L) {
-      lastReconnectAttempt = t;
-      if (mqttConnect()) {
-        lastReconnectAttempt = 0;
-      }
-    }
-    delay(100);
-    return;
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop();
 
-  // GPS Koordinaten von Modul lesen
-  gpsState.originLat = gps.location.lat();
-  gpsState.originLon = gps.location.lng();
-  gpsState.originAlt = gps.altitude.meters();
- 
-  // Aktuelle Position in nichtflüchtigen ESP32-Speicher schreiben
-  long writeValue;
-  writeValue = gpsState.originLat * 1000000;
-  EEPROM_writeAnything(0, writeValue);
-  writeValue = gpsState.originLon * 1000000;
-  EEPROM_writeAnything(4, writeValue);
-  writeValue = gpsState.originAlt * 1000000;
-  EEPROM_writeAnything(8, writeValue);
-  EEPROM.commit(); // erst mit commit() werden die Daten geschrieben
- 
-  gpsState.distMax = 0;
-  gpsState.altMax = -999999;
-  gpsState.spdMax = 0;
-  gpsState.altMin = 999999;
-
-  while (SerialGPS.available() > 0) {
-    gps.encode(SerialGPS.read());
-  }
-   if (gps.satellites.value() > 4) {
-    gpsState.dist = TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), gpsState.originLat, gpsState.originLon);
- 
-    if (gpsState.dist > gpsState.distMax && abs(gpsState.prevDist - gpsState.dist) < 50) {
-      gpsState.distMax = gpsState.dist;
-    }
-    gpsState.prevDist = gpsState.dist;
- 
-    if (gps.altitude.meters() > gpsState.altMax) {
-      gpsState.altMax = gps.altitude.meters();
-    }
- 
-    if (gps.speed.kmph() > gpsState.spdMax) {
-      gpsState.spdMax = gps.speed.kmph();
-    }
- 
-    if (gps.altitude.meters() < gpsState.altMin) {
-      gpsState.altMin = gps.altitude.meters();
-    }
-  }
- 
   long now = millis();
-  if (now - lastMsg > 3000) {
+  if (now - lastMsg > 5000) {
     lastMsg = now;
     
-  
-
-
-    StaticJsonDocument < 256 > JSONbuffer;
-    JsonObject veri = JSONbuffer.createNestedObject();
-    JsonObject relay = JSONbuffer.createNestedObject();
-       
-        relay["messageTemp"] = "on";
-      
-        veri["LAT"] = gps.location.lat();
-        veri["LONG"] = gps.location.lng();
-        veri["SPEED"] = gps.speed.kmph();
-        veri["ALT"] = gps.altitude.meters();
-        veri["LONG"] = gps.location.lng();
-    char JSONmessageBuffer[200];
-    serializeJsonPretty(JSONbuffer, JSONmessageBuffer);
-    Serial.println("Sending message to MQTT topic..");
-    Serial.println(JSONmessageBuffer);
-    if(  mqtt.subscribe(topicOutput)==true ){
-
-    }
-    if (mqtt.publish("v1/devices/me/telemetry", JSONmessageBuffer) == true) {
-      Serial.println("Success sending message");
-    } else {
-      Serial.println("Error sending message");
-    }
-
+    // Temperature in Celsius
+    temperature = bme.readTemperature();   
+    // Uncomment the next line to set temperature in Fahrenheit 
+    // (and comment the previous temperature line)
+    //temperature = 1.8 * bme.readTemperature() + 32; // Temperature in Fahrenheit
     
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    client.publish("esp32/temperature", tempString);
 
-
-  mqtt.loop();
-}}
+    humidity = bme.readHumidity();
+    
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    client.publish("esp32/humidity", humString);
+  }
+}
